3928c1207cfb97702df633aba1adb1f40b397486
ZiyouddinMominov <muminovz2009@gmail.com>
2023-12-24 11:59:48 +0500

    uyga vazifa

---
+++ js/index.js
@@ -0,0 +1,77 @@
+// 1 - MASALA
+
+// function sum(arr) {
+//   let allsum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       allsum += arr[i] ** 2;
+//     }
+//   }
+//   return allsum;
+// }
+// let son = [1, 2, 3, 4, 5, 6];
+// let res = sum(son);
+// console.log(res);
+
+// 2 - MASALA
+
+// function harf(arr) {
+//   let newarr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] === "string" && arr[i].length > 0) {
+//       newarr.push(arr[i][0]);
+//     }
+//   }
+//   return newarr;
+// }
+// let testarr = ["olma", "banan", "anor", 123, "", "behi"];
+// let newarr = harf(testarr);
+// console.log(newarr);
+
+// 3 - MASALA
+
+// function allsum(arg) {
+//   let res = 0;
+//   for (let key in arg) {
+//     if (typeof arg[key] === "number") {
+//       res += arg[key];
+//     }
+//   }
+//   return res;
+// }
+// let testObj = {
+//   a: 10,
+//   b: "salom",
+//   c: 5,
+//   d: true,
+//   e: 7,
+// };
+// let allnum = allsum(testObj);
+// console.log(allnum);
+
+// 4 - MASALA
+
+// function allsum(arg) {
+//   let res = 0;
+//   for (let key in arg) {
+//     if (typeof arg[key] === "number") {
+//       if (arg[key] % 2 == 1) {
+//         res += arg[key];
+//       }
+//     }
+//   }
+//   return res;
+// }
+// let testObj = {
+//   a: 10,
+//   b: "salom",
+//   c: 5,
+//   d: true,
+//   e: 7,
+// };
+// let allnum = allsum(testObj);
+// console.log(allnum);
+
+// 5 - MASALA
+
+// I DON'T KNOW
